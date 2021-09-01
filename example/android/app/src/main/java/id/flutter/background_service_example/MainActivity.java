4012964d2c693c21b6fdd645c6566b88e2e646e0
jin <sj.yu@gsenc.com>
2021-09-01 10:05:10 +0900

    first commit

--- example/android/app/src/main/java/id/flutter/background_service_example/MainActivity.java
+++ example/android/app/src/main/java/id/flutter/background_service_example/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.background_service_example;
+package id.flutter.background_service_example;
 
 import io.flutter.embedding.android.FlutterActivity;
 
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,46 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
 import 'package:background_service/background_service.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  BackgroundService.initialize(onStart);
+
   runApp(MyApp());
+}
+
+void onStart() {
+  WidgetsFlutterBinding.ensureInitialized();
+  final service = BackgroundService();
+  service.onDataReceived.listen((event) {
+    if (event!["action"] == "setAsForeground") {
+      service.setForegroundMode(true);
+      return;
+    }
+
+    if (event["action"] == "setAsBackground") {
+      service.setForegroundMode(false);
+    }
+
+    if (event["action"] == "stopService") {
+      service.stopBackgroundService();
+    }
+  });
+
+  // bring to foreground
+  service.setForegroundMode(true);
+  Timer.periodic(Duration(seconds: 1), (timer) async {
+    if (!(await service.isServiceRunning())) timer.cancel();
+    service.setNotificationInfo(
+      title: "My App Service",
+      content: "Updated at ${DateTime.now()}",
+    );
+
+    service.sendData(
+      {"current_date": DateTime.now().toIso8601String()},
+    );
+  });
 }
 
 class MyApp extends StatefulWidget {
@@ -14,45 +49,73 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await BackgroundService.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
+  String text = "Stop Service";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Service App'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Column(
+          children: [
+            StreamBuilder<Map<String, dynamic>?>(
+              stream: BackgroundService().onDataReceived,
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                final data = snapshot.data!;
+                DateTime? date = DateTime.tryParse(data["current_date"]);
+                return Text(date.toString());
+              },
+            ),
+            ElevatedButton(
+              child: Text("Foreground Mode"),
+              onPressed: () {
+                BackgroundService()
+                    .sendData({"action": "setAsForeground"});
+              },
+            ),
+            ElevatedButton(
+              child: Text("Background Mode"),
+              onPressed: () {
+                BackgroundService()
+                    .sendData({"action": "setAsBackground"});
+              },
+            ),
+            ElevatedButton(
+              child: Text(text),
+              onPressed: () async {
+                var isRunning =
+                await BackgroundService().isServiceRunning();
+                if (isRunning) {
+                  BackgroundService().sendData(
+                    {"action": "stopService"},
+                  );
+                } else {
+                  BackgroundService.initialize(onStart);
+                }
+                if (!isRunning) {
+                  text = 'Stop Service';
+                } else {
+                  text = 'Start Service';
+                }
+                setState(() {});
+              },
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            BackgroundService().sendData({
+              "hello": "world",
+            });
+          },
+          child: Icon(Icons.play_arrow),
         ),
       ),
     );
